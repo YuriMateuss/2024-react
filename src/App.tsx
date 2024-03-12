32fa757571b51748cca2143f8340fd112617a350
Yuri Mateus <105519122+YuriMateuss@users.noreply.github.com>
2024-03-12 17:36:43 -0300

    alterações

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,11 @@ function MyButton() {
 export default function MyApp() {
   return (
     <div>
-      <h1>Bem-vindo ao meu botão</h1>
+      <h1>Primeiro botão</h1>
+      <MyButton />
+      <h1>Segundo botão</h1>
+      <MyButton />
+      <h1>Último botão</h1>
       <MyButton />
     </div>
   );
